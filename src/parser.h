099f793989c20ed739340f3dcad5e9f59a93c55f
Martin Bobčík <bobcik.martin@seznam.cz>
2017-11-23 22:27:54 +0100

    scope-st-list

--- src/parser.h
+++ src/parser.h
@@ -9,6 +9,15 @@ int parse();
 int prog();
 int end_prog();
 int scope_st_list();
+int fun_st_list();
+int scope_stat();
 int param_list();
 int data_type(int* type);
+int param();
+int assign();
+int expression();
+int print_list();
+int scope_if_stat_list();
+int scope_else_stat_list();
+int scope_while_stat_list();
 #endif //SRC_PARSER_H
